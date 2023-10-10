5be3bcd5c957862f4dd11a858e575f485e8d3352
Maxence <mdumontier78@gmail.com>
2023-10-10 11:20:37 +0200

    Init Back

---
+++ src/main/java/com/example/TrackEvent/BackTrackEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrackEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTrackEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTrackEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrackEvent/BackTrackEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrackEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTrackEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
